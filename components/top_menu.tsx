abe3f54b48f3f97f7554b0e2aeae2be9562f1b11
Jared Strebe <jjps@Jareds-MacBook-Pro-2.local>
2023-01-13 09:25:29 -0700

    Trying to add buger menu.

--- components/top_menu.tsx
+++ components/top_menu.tsx
@@ -1,11 +1,17 @@
 import Link from 'next/link';
 import styles from './top_menu.module.sass';
+import { slide as Menu } from 'react-burger-menu'
 
 
 export default function TopMenu({ }) {
   return (
     <div className={styles.container}>
-      Back
+      <Menu>
+        <Link id="home" className="menu-item" href="/">Home</Link>
+        <Link id="user-guide" className="menu-item" href="/help/user-guide">User Guide</Link>
+        <Link id="settings" className="menu-item" href="/help/user-guide">Settings</Link>
+      </Menu>
+      <div>Back</div>
     </div>
   );
 }
